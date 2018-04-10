13c3f189eb862ad59f1aaee6ece80e530f9df066
Nikolai Grigoriev <ngrigoriev@gmail.com>
2018-04-10 08:39:36 -0400

    Fix for issue #21 (#24)
    Add long option names, add constant for "g" option, uncomment previously
    commented code to enable the saving of the graph data.
    
    Signed-off-by: Nikolai Grigoriev <ngrigoriev@newforma.com>

--- src/main/java/com/newforma/titan/schema/SchemaManagerApp.java
+++ src/main/java/com/newforma/titan/schema/SchemaManagerApp.java
@@ -18,15 +18,26 @@ import com.newforma.titan.schema.actions.ReindexAction.IndexingMethod;
 
 public class SchemaManagerApp {
 
+    private static final String OPTION_GRAPH_CONFIG_FILE = "g";
+    private static final String OPTION_GRAPH_CONFIG_FILE_LONG = "graph-config";
     private static final String OPTION_GENERATE_DOCS = "d";
+    private static final String OPTION_GENERATE_DOCS_LONG = "generate-documentation";
     private static final String OPTION_REINDEX_SPECIFIC = "i";
+    private static final String OPTION_REINDEX_SPECIFIC_LONG = "index-name";
     private static final String OPTION_REINDEX_DATA = "r";
+    private static final String OPTION_REINDEX_DATA_LONG = "reindex-data";
     private static final String OPTION_WRITE_TO_DB = "w";
+    private static final String OPTION_WRITE_TO_DB_LONG = "write";
     private static final String OPTION_LOAD_GRAPHML = "l";
+    private static final String OPTION_LOAD_GRAPHML_LONG = "load-data";
     private static final String OPTION_SAVE_GRAPHML = "s";
+    private static final String OPTION_SAVE_GRAPHML_LONG = "save-data";
     private static final String OPTION_FILTER_TAGS = "t";
+    private static final String OPTION_FILTER_TAGS_LONG = "tag-filter";
     private static final String OPTION_INDEXING_METHOD = "m";
+    private static final String OPTION_INDEXING_METHOD_LONG = "index-method";
     private static final String OPTION_REINDEX_TIMEOUT = "it";
+    private static final String OPTION_REINDEX_TIMEOUT_LONG = "index-wait-timeout";
 
     private static final Logger LOG = LoggerFactory.getLogger(SchemaManagerApp.class);
 
@@ -51,7 +62,7 @@ public class SchemaManagerApp {
         }
 
         final boolean doApplyChanges = cmdLine.hasOption(OPTION_WRITE_TO_DB);
-        final String graphConfigFile = cmdLine.getOptionValue("g");
+        final String graphConfigFile = cmdLine.getOptionValue(OPTION_GRAPH_CONFIG_FILE);
         final List<ReindexAction> reindexActions = new LinkedList<>();
         final IndexingMethod indexingMethod;
         if (cmdLine.hasOption(OPTION_INDEXING_METHOD)) {
@@ -84,7 +95,7 @@ public class SchemaManagerApp {
                     .applyTagFilter(tagFilter).andGenerateDocumentation(docDir)
                     .andLoadData(graphMLToLoad)
                     .reindexingTimeout(reindexTimeoutInSecs)
-                    ./*andSaveData(graphMLToSave).*/run();
+                    .andSaveData(graphMLToSave).run();
         } catch (Throwable t) {
             LOG.error("ERROR", t);
             System.exit(1);
@@ -95,22 +106,22 @@ public class SchemaManagerApp {
 
     private static Options populateOptions() {
         final Options options = new Options();
-        options.addOption(OPTION_WRITE_TO_DB, false, "Write the relations defined by the schema to the graph");
-        options.addOption(OPTION_REINDEX_DATA, true, "Reindex data: ALL, NEW, UNAVAILABLE");
-        options.addOption(OPTION_REINDEX_SPECIFIC, true, "Reindex the specific index after applying the schema");
-        options.addOption(OPTION_INDEXING_METHOD, true, "Using the specific indexing method: one of " +
+        options.addOption(OPTION_WRITE_TO_DB, OPTION_WRITE_TO_DB_LONG, false, "Write the relations defined by the schema to the graph");
+        options.addOption(OPTION_REINDEX_DATA, OPTION_REINDEX_DATA_LONG, true, "Reindex data: ALL, NEW, UNAVAILABLE");
+        options.addOption(OPTION_REINDEX_SPECIFIC, OPTION_REINDEX_SPECIFIC_LONG, true, "Reindex the specific index after applying the schema");
+        options.addOption(OPTION_INDEXING_METHOD, OPTION_INDEXING_METHOD_LONG, true, "Using the specific indexing method: one of " +
                 StringUtils.join(ReindexAction.IndexingMethod.values(), ',') + " ("  +
                 ReindexAction.IndexingMethod.LOCAL + " is the default)");
-        options.addOption(OPTION_GENERATE_DOCS, true, "Generate documentation, write to the specified directory");
-        options.addOption(OPTION_LOAD_GRAPHML, true, "Load specific GraphML file into the database");
-        options.addOption(OPTION_SAVE_GRAPHML, true, "Save specific GraphML file into a file");
-        options.addOption(OPTION_FILTER_TAGS, true, "Apply tag filter for generated documentation. "
+        options.addOption(OPTION_GENERATE_DOCS, OPTION_GENERATE_DOCS_LONG, true, "Generate documentation, write to the specified directory");
+        options.addOption(OPTION_LOAD_GRAPHML, OPTION_LOAD_GRAPHML_LONG, true, "Load specific GraphML file into the database");
+        options.addOption(OPTION_SAVE_GRAPHML, OPTION_SAVE_GRAPHML_LONG, true, "Save specific GraphML file into a file");
+        options.addOption(OPTION_FILTER_TAGS, OPTION_FILTER_TAGS_LONG, true, "Apply tag filter for generated documentation. "
                 + "Filter format: tag-spec[,tag-spec[,...]]. tag-spec ::= [!]tag-name[:tag-color]. "
                 + "Colors are used for DOT diagram. If the filter is specified, then only the elements having the "
                 + "specified tags will be included in the documentation and the elements having the tags prefixed with "
                 + "\"!\" will be excluded.");
-        options.addRequiredOption("g", "graph-config", true, "Graph property file name");
-        options.addOption(OPTION_REINDEX_TIMEOUT, true, "Specify the amount of time in seconds to wait before timing out on an index creation. Default 300 seconds.");
+        options.addRequiredOption(OPTION_GRAPH_CONFIG_FILE, OPTION_GRAPH_CONFIG_FILE_LONG, true, "Graph property file name");
+        options.addOption(OPTION_REINDEX_TIMEOUT, OPTION_REINDEX_TIMEOUT_LONG, true, "Specify the amount of time in seconds to wait before timing out on an index creation. Default 300 seconds.");
         return options;
     }
 
